18be67ac50c399b9e23a35d56d6a6a96b38d5ee3
alex <alejandro.fa.daw@gmail.com>
2017-06-07 16:08:18 +0200

    Envío de form de contacto. Hay que probar en hostinger.

---
+++ Personalizame/Tienda/application/controllers/contact.php
@@ -0,0 +1,31 @@
+<?php
+// Free Bootstrap Themes : http://www.365bootstrap.com
+class Color extends CI_Controller{
+	public function formContacto(){
+		$text = "<span style='color:red; font-size: 35px; line-height: 40px; magin: 10px;'>Error! Por favor, inténtalo de nuevo.</span>";
+		
+		if(isset($_POST['name']))
+		{
+			$name=$_POST['name'];
+			$email=$_POST['email'];
+			$message=$_POST['message'];
+		
+			$to = "alejandro.fa.daw@gmail.com";
+			$subject = "Personalízame - Formulario de Contacto";
+			$message = " Nombre: " . $name ."\r\n Email: " . $email . "\r\n Mensaje:\r\n" . $message;
+			 
+			$from = "365Bootstrap.com";
+			$headers = "From:" . $from . "\r\n";
+			$headers .= "Content-type: text/plain; charset=UTF-8" . "\r\n"; 
+			 
+			if(@mail($to,$subject,$message,$headers))
+			{
+			  $text = "<span style='color:blue; font-size: 35px; line-height: 40px; margin: 10px;'>Tu mensaje fue enviado!</span>";
+			}
+		}
+		
+		
+		
+	}
+}
+?>
